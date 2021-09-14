c6fb3339edd509ee20f9f21172b76b49a7472b2f
ssofnh01@lw <970954320@qq.com>
2021-09-14 21:06:21 +0800

    第一次提交demo

---
+++ Config/__init__.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+#@time   :2018/12/27 22:19
+#@Author :Keep Learing
+#@boke   :www.cnblogs.com/fighter007
+#@motto  :Keep Learing
+
+
+'''
+
+Config目录：存放配置文件，如数据库的端口，地址和邮件信息等。
+Data目录：存放公共动态数据，如token、Excel、动态参数等。
+Log目录：存放Log日志信息。
+Report目录：存放接口测试报告。
+TestCases目录：存放接口测试案例。
+Utlis目录：公共方法、自定义工具类所在目录。
+runMain.py文件：项目运行的主程序文件。
+
+
+'''
